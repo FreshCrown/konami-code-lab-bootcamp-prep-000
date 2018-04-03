a1489d0b2c9fb07b7937e9f64d6b4beba374f675
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-03 19:00:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  document.addEventListener('keydown', (events))
+  document.body.addEventListener('keydown', (events))
   
 }
